38828b3775be7c5ca31da6ca935835e553d7e85a
Andrés Catalán <hola@andres.cat>
2024-10-11 13:08:06 -0400

    add Salesforce CLI formula

---
+++ Formula/sf-cli.rb
@@ -0,0 +1,20 @@
+class SalesforceCli < Formula
+    desc "An example formula"
+    homepage "https://example.com"
+    url "https://registry.npmjs.org/@salesforce/cli/-/cli-2.61.8.tgz"
+    sha256 "df80fec6fd420f40a703ece40718a1d6f22cafee"
+  
+    depends_on "node"
+    # uncomment if there is a native addon inside the dependency tree
+    # depends_on "python" => :build
+  
+    def install
+      system "npm", "install", *std_npm_args
+      bin.install_symlink Dir["#{libexec}/bin/*"]
+    end
+  
+    test do
+      # add a meaningful test here, version isn't usually meaningful
+      assert_match version.to_s, shell_output("#{bin}/foo --version")
+    end
+  end
